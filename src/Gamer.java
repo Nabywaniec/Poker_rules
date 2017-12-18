8e5dddc09a974b6c20b5182d06c0b81e502adf02
Nabywaniec <Nabywaniec@users.noreply.github.com>
2017-12-18 15:30:23 +0100

    Create Gamer.java

---
+++ src/Gamer.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Gamer {
+
+    private int id;
+    private ArrayList<Bone> bones = new ArrayList<>();
+
+    public Gamer(int id) {
+        this.id = id;
+        this.bones.clear();
+    }
+
+    public void changeBones(Bone t[], int n){
+
+        for(int i=0;i<n;i++){
+            this.bones.remove(t[i]);
+        }
+
+        this.bones.addAll(BonesManager.getBones(n));
+    }
+
+
+
+}
